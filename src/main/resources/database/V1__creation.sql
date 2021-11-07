dc490708d238e4fbad3a78414ba989fed44fb8bb
xendv <dvoynina.ksusha@mail.ru>
2021-11-07 13:13:50 +0300

    Decomposed into assigned_teachers, lesson, students_group, several logical fixes

--- src/main/resources/database/V1__creation.sql
+++ src/main/resources/database/V1__creation.sql
@@ -24,22 +24,40 @@ CREATE TABLE course
     CONSTRAINT course_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE schedule
+CREATE TABLE students_group
 (
     id SERIAL NOT NULL,
-    course_id  INT NOT NULL REFERENCES course (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    teacher_id INT NOT NULL REFERENCES teacher (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
-    theme VARCHAR NOT NULL,
-    CONSTRAINT schedule_pk PRIMARY KEY (id)
+    course_id INT NOT NULL REFERENCES course (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    name VARCHAR NOT NULL,
+    CONSTRAINT group_pk PRIMARY KEY (id)
 );
 
 CREATE TABLE enrollment
 (
-    course_id INT NOT NULL REFERENCES course (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    group_id INT NOT NULL REFERENCES students_group (id) ON UPDATE CASCADE ON DELETE CASCADE,
     student_id INT NOT NULL REFERENCES student (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    CONSTRAINT enrollment_pk PRIMARY KEY (course_id, student_id)
+    CONSTRAINT enrollment_pk PRIMARY KEY (group_id, student_id)
 );
 
+CREATE TABLE lesson
+(
+    id SERIAL NOT NULL,
+    theme VARCHAR NOT NULL,
+    CONSTRAINT lesson_pk PRIMARY KEY (id)
+);
 
+CREATE TABLE schedule
+(
+    id SERIAL NOT NULL,
+    group_id  INT NOT NULL REFERENCES students_group (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    lesson_id  INT NOT NULL REFERENCES lesson (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    date TIMESTAMP WITHOUT TIME ZONE,
+    CONSTRAINT schedule_pk PRIMARY KEY (id)
+);
 
+CREATE TABLE assigned_teachers
+(
+    schedule_id  INT NOT NULL REFERENCES schedule (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    teacher_id INT NOT NULL REFERENCES teacher (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT assigned_teachers_pk PRIMARY KEY (schedule_id, teacher_id)
+);
